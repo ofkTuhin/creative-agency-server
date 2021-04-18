422aab0b1d18dac9c0f60896af9acd3091e4cad3
omar-faruk02 <tuhin.omar99@gmail.com>
2021-04-18 04:19:54 -0800

    2nd

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const bodyParser=require('body-parser')
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config()
-port=5000
+const port =process.env.PORT||5000
 
-const uri = "mongodb+srv://omar:faruk12345@cluster0.gqnwo.mongodb.net/cerativeAgency?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gqnwo.mongodb.net/cerativeAgency?retryWrites=true&w=majority`;
 
-
+console.log(uri)
 
 
 const app = express()
@@ -28,6 +28,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const reviewCollection = client.db("cerativeAgency").collection("review");
   const serviceCollection = client.db("cerativeAgency").collection("service");
+  const bookingCollection = client.db("cerativeAgency").collection("booking");
+  const adminCollection = client.db("cerativeAgency").collection("admin");
   app.post('/addReview',(req,res)=>{
     console.log(req.body)
     reviewCollection.insertOne(req.body)
@@ -46,6 +48,16 @@ client.connect(err => {
     serviceCollection.insertOne(req.body)
 
   })
+
+  app.delete('/delete/:id', (req, res) => {
+    console.log( ObjectId(req.params.id))
+    serviceCollection.deleteOne({ _id: ObjectId(req.params.id) })
+      .then(result => { 
+        res.send(result.deletedCount>0)
+       })
+      
+  })
+
   app.get('/orderService/:id', (req, res) => {
     console.log(req.params.id)
     
@@ -68,7 +80,48 @@ client.connect(err => {
     })
   })
 
+  app.post('/serviceBooking',(req,res)=>{
+    console.log(req.body)
+    bookingCollection.insertOne(req.body)
+
+  })
+
+  app.post('/addAdmin',(req,res)=>{
+    console.log(req.body)
+    adminCollection.insertOne(req.body)
+
+  })
+
+
+  app.post('/orderList', (req, res) => {
+    
+    const email = req.body.email;
+    adminCollection.find({ email: email })
+        .toArray((err, admin) => {
+            const filter = {}
+            if (admin.length === 0) {
+                filter.email = email;
+            }
+            bookingCollection.find(filter)
+                .toArray((err, documents) => {
+                   
+                    res.send(documents);
+                })
+        })
+})
   // perform actions on the collection object
+
+  
+   
+ 
+
+  app.post('/isAdmin', (req, res) => {
+    const email = req.body.email;
+    adminCollection.find({ email: email })
+        .toArray((err, admin) => {
+            res.send(admin.length > 0);
+        })
+})
  
 });
 
